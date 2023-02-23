02ef76862cbb47b725fa51a48238c0b4720cd78f
naoehcleber <joao.vr.fernandes@gmail.com>
2023-02-23 18:52:15 -0300

    Update Predios.c
    mudei basicamente tudo q eu tinha feito, antes eu não fiz quase nada e dessa vez eu segui um tutorial ao invés de fazer meio freestyle. Tem erros gritantes na execução que eu vou mexer depois

--- Predios/Predios.c
+++ Predios/Predios.c
@@ -2,24 +2,53 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#include "funcoes.h"
+//#include "funcoes.h"
+
+int y, x, i,altura_predio;
+// x é a largura máxima da tela
+// y é a altura maáxima da tela
+//i e altura_predio são usadas nas funções de gerar o predio.
+
+void setup(){
+    keypad(stdscr, true);
+    noecho();
+    getmaxyx(stdscr, y, x);
+}
+
 
 void main(){
-    // começa o curses
     initscr();
-    keypad(stdscr, true); //Para o programa reconhecer as setas do teclado.
-    //define o y e o x da tela
-    int y, x, yMax, xMax;
+    setup();
+    int height_game, width_game, start_y, start_x;
+    height_game = 20;
+    width_game = x;
+    start_x = 0;
+    start_y = 10;
+
+    WINDOW * win = newwin(height_game, width_game, start_y, start_x);
+    refresh();
 
 
-    getyx(stdscr, y, x);
-    //pega o bvalor máximo de x e y da janela
-    getmaxyx(stdscr, yMax, xMax);
-    printf(AlturaAleatoria)
+    box(win, 0, 0); //os numeros nessa função são os unicode pra os simbolos, 0 é um traço.
+    for(q>15; q < height_game; q++){
+        for(i>0; i< width_game; i++){
+            mvwprintw(win,q,i, "P");
+            wrefresh(win); //faz refresh so da window
+        }
+    }
+    //raw();
 
 
+    //int linha, coluna;
+    //for (linha=0; linha < x; ++linha ){
+    //    addch('P');
+    //    refresh();
+    //}
+    //refresh();
 
+    int c = getch();
     endwin();
-    return 0;
+return ;
 }
+
 
